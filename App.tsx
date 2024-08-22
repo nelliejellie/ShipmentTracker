b9f6ea48894123004f2f3f1f6a62220457255abf
nelliejellie <emekaewelike@gmail.com>
2024-08-22 17:57:07 +0100

    toast installed

--- App.tsx
+++ App.tsx
@@ -2,9 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import StackNavigator from "@/Navigation/StackNavigator";
+import Toast from "react-native-toast-message";
 
 export default function App() {
-  return <StackNavigator />;
+  return (
+    <View>
+      <StackNavigator />
+      <Toast />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
